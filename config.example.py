e7800894807b35311c5d9cbab22b9d69637f638b
ForceCore <ssanzing@gmail.com>
2019-04-13 20:58:24 +0900

    Rename config.py as config.example.py
    This will help me avoid committing my info to github

---
+++ config.example.py
@@ -0,0 +1,5 @@
+# Example configuration file.
+# Copy and rename this file as config.py and input your settings.
+
+DEBUG = False  # If True, Cybos will not initialize.
+CYBOS_TRADING_PASSWORD = "0302"
